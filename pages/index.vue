f5b395f34a34379eb077600aa3f8f523f539361f
Chris Elston <celston@onenorth.com>
2018-07-10 08:17:47 -0500

    Fleshing out rating submit form

--- pages/index.vue
+++ pages/index.vue
@@ -27,103 +27,81 @@
     </b-navbar>
 
     <main role="main" class="container">
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
-      <p>The quick brown fox jumped over the lazy dog.</p>
+      <h1>Submit New Rating</h1>
+
+      <div v-if="!success">
+        <b-form @submit="formSubmit" novalidate>
+          <b-form-group label="Brand" label-for="brand" :state="brandState" :invalid-feedback="'Brand is required.'">
+            <b-form-input id="brand" v-model.trim="form.brand" required :state="brandState" placeholder="Who makes, sells or serves this product?" />
+          </b-form-group>
+
+          <b-form-group label="Name" label-for="name">
+            <b-form-input id="name" v-model.trim="form.name" required />
+          </b-form-group>
+
+          <b-form-group label="Rating" label-for="rating" description="Give a score 1-5">
+            <b-form-input id="rating" v-model="form.rating" class="form-control-range" type="range" min="0" max="5" step="0.5" />
+          </b-form-group>
+
+          <p>{{form.rating}}</p>
+
+          <b-button type="submit" variant="primary" v-bind:disabled="submitting">Submit</b-button>&nbsp;
+        </b-form>
+      </div>
+      <div v-else>
+        <p>Thanks for your submission!</p>
+      </div>
     </main>
   </div>
 </template>
 
 <script>
-export default {
 
+const initialBrand = 'test'
+
+export default {
+  data() {
+    return {
+      form: {
+        brand: '',
+        name: '',
+        rating: 2.5
+      },
+      submitted: false,
+      submitting: false,
+      success: false,
+    }
+  },
+  methods: {
+    formSubmit (evt) {
+      evt.preventDefault()
+
+      console.log(evt)
+      console.log(this.form)
+
+      this.submitted = true
+      this.submitting = true
+    },
+    formReset (evt) {
+      this.form.brand = initialBrand
+    }
+  },
+  computed: {
+    brandInvalid() {
+      if (!this.submitted) {
+        return false
+      }
+
+      return this.form.brand.length == 0
+    },
+    brandState () {
+      if (!this.submitted) {
+        return null
+      }
+
+      return this.form.brand.length > 0
+    }
+  }
 }
 </script>
 
